efd93564a4b01c79a8ab973ff328a8420131d584
Ogaboga1 <emilaamodt@live.no>
2025-12-09 10:22:55 +0100

    while loop  & win condition at the end

--- Krokodille spill/Program.cs
+++ Krokodille spill/Program.cs
@@ -5,41 +5,46 @@ class Numbers
     int Number1 = 0;
     int Number2 = 5;
     int Count = 0;
+    Random rng = new Random();
     public void Rng()
     {
-        Random rng = new Random();
         Number1 = rng.Next(1,11);
         Number2 = rng.Next(1,11);
     }
     public void Run()
     {
-        Rng();
-        Console.WriteLine("Skriv >,<, eller =");
-        Console.WriteLine($"{Number1} _ {Number2}");
-        var input = Console.ReadLine();
-        if (Count >= 9) 
-        { 
-            Console.Write("Du vant");
-            return;
-        }
-        bool correct = input switch
+        while (true)
         {
-            "<" => Number1 < Number2,
-            ">" => Number1 > Number2,
-            "=" => Number1 == Number2,
-            _ => false
-        };
-        if (correct) 
-        {
-            Count++;
-            Console.WriteLine($"Det var riktig. Du har nå {Count} poeng");
+
+            Rng();
+            Console.WriteLine("Skriv >,<, eller =");
+            Console.WriteLine($"{Number1} _ {Number2}");
+
+            var input = Console.ReadLine();
+
+            bool correct = input switch
+            {
+                "<" => Number1 < Number2,
+                ">" => Number1 > Number2,
+                "=" => Number1 == Number2,
+                _ => false
+            };
+            if (correct)
+            {
+                Count++;
+                Console.WriteLine($"Det var riktig. Du har nå {Count} poeng");
+            }
+            else
+            {
+                Count = 0;
+                Console.WriteLine("Det var feil. Prøv igjen");
+            }
+            if (Count >= 10)
+            {
+                Console.Write("Du vant");
+                break;
+            }
         }
-        else
-        {
-            Count = 0;
-            Console.WriteLine("Det var feil. Prøv igjen");
-        }
-        Run();
     }
 
     class Program
